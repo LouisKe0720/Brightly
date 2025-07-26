475f2eea9bf18bc473cfd090e45767e9b03ffff7
Louis Ke <louiske720@gmail.com>
2025-07-25 21:19:38 -0400

    Navigation Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import BrightlyLogo from '../assets/Brightly.png';
 
 function Navbar() {
+    // Used to determine the current active route
+    const location = useLocation();
+
+    // Used to navigate through routes
+    const navigate = useNavigate();
+
+    // Function when logo is clicked - goes to home page
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
     return (
         <nav>
+            {/* Navbar container with logo and links */}
             <div className="navbar-container">
-                <div className="logo"><img src={BrightlyLogo} alt="StudyBuddy Logo" /></div>
+                <div className="logo">
+                    <img 
+                        src={BrightlyLogo} 
+                        alt="StudyBuddy Logo" 
+                        onClick={handleLogoClick}
+                        style={{ cursor: 'pointer' }}
+                    />
+                </div>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li>
+                        {/* Home link with active class based on current route */}
+                        <Link 
+                            to="/" 
+                            className={location.pathname === '/' ? 'active' : ''}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        {/* Dashboard link with active class based on current route */}
+                        <Link 
+                            to="/dashboard" 
+                            className={location.pathname === '/dashboard' ? 'active' : ''}
+                        >
+                            Dashboard
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
